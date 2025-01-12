80c053b714720647942f2a50b668fef507e3d22b
lavanderhoney <milapp857@gmail.com>
2025-01-12 21:16:38 +0530

    printing edge labels

--- graph_base.py
+++ graph_base.py
@@ -5,23 +5,26 @@ from matplotlib import pyplot as plt
 
 
 class GraphView():
-    def __init__(self, V, E, edges, is_undirected=True, is_weighted=False):
+    def __init__(self, V, E, edges, is_undirected=True, is_weighted=False, is_labelled=False):
         """
         Initialize the graph view.
         V: number of vertices
         E: number of edges
         edges: list of edges
-        is_undirected: True if the graph is undirected, False otherwise
-        is_weighted: True if the graph is weighted, False otherwise
+        is_undirected: (default=True) True if the graph is undirected, False otherwise
+        is_weighted: (default=False) True if the graph is weighted, False otherwise
+        is_labelled: (default=False) True if the graph is labelled, False otherwise
         """
-        self.V = V # number of vertices
-        self.E = E # number of edges
-        self.edges_list = edges # list of edges
-        self.last_el_ptr = 0 # pointer to the last element in the edge list
-        self.is_undirected = is_undirected # True if the graph is undirected(default=True)
-        self.is_weighted = is_weighted # True if the graph is weighted(default=False)
+        self.V = V  # number of vertices
+        self.E = E  # number of edges
+        self.edges_list = edges  # list of edges
+        self.last_el_ptr = 0  # pointer to the last element in the edge list
+        self.is_undirected = is_undirected  # True if the graph is undirected
+        self.is_weighted = is_weighted  # True if the graph is weighted
+        self.is_labelled = is_labelled
         self.adjacency_list = [[] for _ in range(self.V)]
-        self.adjacency_matrix = [[0 for _ in range(self.V)] for __ in range(self.V)]
+        self.adjacency_matrix = [
+            [0 for _ in range(self.V)] for __ in range(self.V)]
         self.incidence_matrix = [[] for __ in range(self.V)]
         self.update_all_views(1)
         self.last_el_ptr += self.E
@@ -30,15 +33,16 @@ class GraphView():
         print("Adjacency List:")
         for i in range(self.V):
             print(f"{i}: {self.adjacency_list[i]}")
-    
+
     def print_adjacency_matrix(self):
         print("Adjacency Matrix:")
         print(np.array(self.adjacency_matrix))
-    
+
     def print_incidence_matrix(self):
         print("Incidence Matrix:")
         print(np.array(self.incidence_matrix))
-    #provide lists of edges to delete, check same for matrix as well
+    # provide lists of edges to delete, check same for matrix as well
+
     def update_adjacency_list(self, operation, del_edges=[]):
         """
         Update the adjacency list after adding an edge.
@@ -46,22 +50,25 @@ class GraphView():
         """
         if operation == 1:
             # this for loop goes from the last(previous) edge added to the end of the new extended edge_list
-            weight=1
+            weight = 1
             for i in range(self.last_el_ptr, len(self.edges_list)):
                 if self.is_weighted:
                     weight = self.edges_list[i][2]
-                self.adjacency_list[self.edges_list[i][0]].append((self.edges_list[i][1], weight))
+                self.adjacency_list[self.edges_list[i][0]].append(
+                    (self.edges_list[i][1], weight))
                 if self.is_undirected:
-                    self.adjacency_list[self.edges_list[i][1]].append((self.edges_list[i][0], weight))
+                    self.adjacency_list[self.edges_list[i][1]].append(
+                        (self.edges_list[i][0], weight))
             # print(f"\nNew adj_list: {self.print_adjacency_list()}. \n Time = O(1)") #make display function to print properly
         else:
             for i in range(self.E, len(del_edges)):
                 self.adjacency_list[del_edges[i][0]].remove(del_edges[i][1])
                 if self.is_undirected:
-                    self.adjacency_list[del_edges[i][1]].remove(del_edges[i][0])
+                    self.adjacency_list[del_edges[i]
+                                        [1]].remove(del_edges[i][0])
             # print(f"\nNew adj_list: {self.print_adjacency_list()}. \n Time = O(V)")
-    
-    def update_adjacency_matrix(self,operation, del_edges=[]):
+
+    def update_adjacency_matrix(self, operation, del_edges=[]):
         """
         Update the adjacency matrix after adding an edge.
         operation: 1 for adding an edge, 0 for deleting an edge
@@ -72,9 +79,11 @@ class GraphView():
                     weight = self.edges_list[i][2]
                 else:
                     weight = 1
-                self.adjacency_matrix[self.edges_list[i][0]][self.edges_list[i][1]] = weight
+                self.adjacency_matrix[self.edges_list[i]
+                                      [0]][self.edges_list[i][1]] = weight
                 if self.is_undirected:
-                    self.adjacency_matrix[self.edges_list[i][1]][self.edges_list[i][0]] =  weight
+                    self.adjacency_matrix[self.edges_list[i]
+                                          [1]][self.edges_list[i][0]] = weight
             # print(f"\nNew adjacency_matrix: {self.print_adjacency_matrix()}. \n Time = O(1)")
         else:
             for i in range(len(del_edges)):
@@ -87,7 +96,8 @@ class GraphView():
         Update the incidence matrix after adding an edge.
         operation: 1 for adding an edge, 0 for deleting an edge
         """
-        self.incidence_matrix = [[0 for _ in range(len(self.edges_list))] for __ in range(self.V)]
+        self.incidence_matrix = [
+            [0 for _ in range(len(self.edges_list))] for __ in range(self.V)]
 
         if operation == 1:
             for i in range(len(self.edges_list)):
@@ -100,21 +110,23 @@ class GraphView():
                 self.incidence_matrix[del_edges[i][0]][edge_to_del] = 0
                 self.incidence_matrix[del_edges[i][1]][edge_to_del] = 0
             # print(f"\nNew incidence_matrix: {self.print_incidence_matrix()}. \n Time = O(E)")
-     
+
     def update_all_views(self, operation, del_edges=[]):
         """
         Sorts the edges list and then Update all the views after adding an edge.
         operation: 1 for adding an edge, 0 for deleting an edge
         """
-        self.edges_list.sort(key=lambda x: x[0]) # sort the edges list: O(ElogE)
+        self.edges_list.sort(
+            key=lambda x: x[0])  # sort the edges list: O(ElogE)
         self.update_adjacency_list(operation, del_edges)
         self.update_adjacency_matrix(operation, del_edges)
         self.update_incidence_matrix(operation, del_edges)
 
+
 class Graph(GraphView):
-    def __init__(self,*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    
+
     def add_edge(self, edge):
         """
         Add an edge to the graph.
@@ -136,7 +148,7 @@ class Graph(GraphView):
         self.edges_list.extend(edges)
         self.update_all_views(1)
         self.E += len(edges)
-    
+
     def delete_edges(self, del_edges):
         """
         Delete an edge from the graph.
@@ -152,22 +164,42 @@ class Graph(GraphView):
     def print_graph_nx(self):
         if self.is_weighted:
             edges = [(edge[0], edge[1]) for edge in self.edges_list]
+        elif self.is_labelled:
+            edges = []
+            edge_labels = dict()
+            for edge in self.edges_list:
+                edges.append((edge[0], edge[1]))
+                edge_labels[(edge[0], edge[1])] = edge[2]
+            print("edge labels: ", edge_labels)
         else:
             edges = self.edges_list
+
         G = nx.from_edgelist(edges)
-        nx.draw(G, with_labels=True, node_color='skyblue', node_size=2000, edge_color='gray', font_size=15, font_color='black')
+        pos = nx.spring_layout(G)
+        plt.figure()
+        nx.draw(G, pos, with_labels=True, node_color='skyblue',
+                node_size=2000, edge_color='gray', font_size=15, font_color='black')
+        if self.is_labelled:
+            nx.draw_networkx_edge_labels(
+                G, pos,
+                edge_labels=edge_labels,
+                font_color="red"
+            )
+        plt.axis("off")
         plt.title("Graph from Adjacency Matrix")
         plt.show()
 
+
 if __name__ == "__main__":
     # Example usage
-    g1 = Graph(4, 6, [(0, 1),(1, 2), (2, 3),(3,0), (0,2),(1,3)], is_undirected=True, is_weighted=False)
+    g1 = Graph(4, 6, [(0, 1), (1, 2), (2, 3), (3, 0), (0, 2),
+               (1, 3)], is_undirected=True, is_weighted=False)
     g1.print_graph_nx()
 
-    g1.add_edge((1,3))
+    g1.add_edge((1, 3))
 
     g1.print_graph_nx()
 
-    g1.delete_edges([(1,3)])
+    g1.delete_edges([(1, 3)])
 
-    g1.print_graph_nx()  
+    g1.print_graph_nx()
--- graph_ops.py
+++ graph_ops.py
@@ -1,45 +1,46 @@
 from collections import defaultdict
 from graph_base import *
 
+
 class GraphOps():
 
-    #assume the vertices of G1 and G2 will have same labelling or numbering, like 0,1,2 etc...
+    # assume the vertices of G1 and G2 will have same labelling or numbering, like 0,1,2 etc...
 
     def __init__(self):
         pass
-    
-    def get_adjacency_list(self,edges_list):
+
+    def get_adjacency_list(self, edges_list):
         adj_list = defaultdict(set)
         for edge in edges_list:
             adj_list[edge[0]].add(edge[1])
             adj_list[edge[1]].add(edge[0])
         return dict(adj_list)
 
-    def get_edges_list(self,adj_list):
+    def get_edges_list(self, adj_list):
         edges_list = []
         for node, neighbors in adj_list.items():
             for neighbor in neighbors:
-                edges_list.append((node,neighbor[0]))
+                edges_list.append((node, neighbor[0]))
         return edges_list
-    
-    def union(self,g1, g2):
+
+    def union(self, g1, g2):
         """Computes the union of two graphs represented as adjacency lists.
         g1: Graph object (from graph_base.py) of graph 1
         g2: Graph object (from graph_base.py) of graph 2
 
         returns: R, a Graph object of the union of g1 and g2
         """
-        g1_adjlist = g1.adjacency_list 
+        g1_adjlist = g1.adjacency_list
         g2_adjlist = g2.adjacency_list
 
-        #Find the number of vertices in the union graph
-        R_vertices = set()  
+        # Find the number of vertices in the union graph
+        R_vertices = set()
         for node, edges in enumerate(g1_adjlist):
             R_vertices.add(node)
         for node, edges in enumerate(g2_adjlist):
             R_vertices.add(node)
 
-        R_adjlist = [set() for _ in range(len(R_vertices))] #list of sets
+        R_adjlist = [set() for _ in range(len(R_vertices))]  # list of sets
         for node, edges in enumerate(g1_adjlist):
             for edge in edges:
                 R_adjlist[node].add(edge)
@@ -50,15 +51,16 @@ class GraphOps():
         R_adjlist = dict(enumerate(R_adjlist))
         R_edges = self.get_edges_list(R_adjlist)
         R = Graph(
-            V = len(R_vertices),
-            E = len(R_edges),
-            is_weighted = g1.is_weighted,
-            is_undirected = g1.is_undirected,
-            edges= R_edges
+            V=len(R_vertices),
+            E=len(R_edges),
+            is_weighted=g1.is_weighted,
+            is_undirected=g1.is_undirected,
+            edges=R_edges,
+            is_labelled=True
         )
 
         return R
-    
+
     def intersection(self, g1, g2):
         """Computes the R of two graphs represented as adjacency lists.
         g1: Graph object (from graph_base.py) of graph 1
@@ -66,17 +68,17 @@ class GraphOps():
 
         returns: R, a Graph object of the intersection of g1 and g2
         """
-        g1_adjlist = g1.adjacency_list 
+        g1_adjlist = g1.adjacency_list
         g2_adjlist = g2.adjacency_list
 
-        #Find the common vertices
-        R_vertices = set()  
+        # Find the common vertices
+        R_vertices = set()
         for node, edges in enumerate(g1_adjlist):
             R_vertices.add(node)
         for node, edges in enumerate(g2_adjlist):
             R_vertices.add(node)
-        
-        R_adjlist = [set() for _ in range(len(R_vertices))] #list of sets
+
+        R_adjlist = [set() for _ in range(len(R_vertices))]  # list of sets
         for node, edges in enumerate(g1_adjlist):
             for edge in edges:
                 if edge in g2_adjlist[node]:
@@ -85,15 +87,16 @@ class GraphOps():
         R_adjlist = dict(enumerate(R_adjlist))
         R_edges = self.get_edges_list(R_adjlist)
         R = Graph(
-            V = len(R_vertices),
-            E = len(R_edges),
-            is_weighted = g1.is_weighted,
-            is_undirected = g1.is_undirected,
-            edges= R_edges
+            V=len(R_vertices),
+            E=len(R_edges),
+            is_weighted=g1.is_weighted,
+            is_undirected=g1.is_undirected,
+            edges=R_edges,
+            is_labelled=True
         )
 
         return R
-    
+
     def difference(self, g1, g2):
         """Computes the difference of two graphs represented as adjacency lists.
         g1: Graph object (from graph_base.py) of graph 1
@@ -101,17 +104,17 @@ class GraphOps():
 
         returns: R, a Graph object of the difference of g1 and g2
         """
-        g1_adjlist = g1.adjacency_list 
+        g1_adjlist = g1.adjacency_list
         g2_adjlist = g2.adjacency_list
 
-        #Find the number of vertices in the difference graph
-        R_vertices = set()  
+        # Find the number of vertices in the difference graph
+        R_vertices = set()
         for node, edges in enumerate(g1_adjlist):
             R_vertices.add(node)
         for node, edges in enumerate(g2_adjlist):
             R_vertices.add(node)
-        
-        R_adjlist = [set() for _ in range(len(R_vertices))] #list of sets
+
+        R_adjlist = [set() for _ in range(len(R_vertices))]  # list of sets
         for node, edges in enumerate(g1_adjlist):
             for edge in edges:
                 if edge not in g2_adjlist[node]:
@@ -120,15 +123,16 @@ class GraphOps():
         R_adjlist = dict(enumerate(R_adjlist))
         R_edges = self.get_edges_list(R_adjlist)
         R = Graph(
-            V = len(R_vertices),
-            E = len(R_edges),
-            is_weighted = g1.is_weighted,
-            is_undirected = g1.is_undirected,
-            edges = R_edges
+            V=len(R_vertices),
+            E=len(R_edges),
+            edges=R_edges,
+            is_weighted=g1.is_weighted,
+            is_undirected=g1.is_undirected,
+            is_labelled=True
         )
         return R
-    
-    def ringsum(self,g1, g2):
+
+    def ringsum(self, g1, g2):
         """Computes the ringsum of two graphs represented as adjacency lists.
         g1: Graph object (from graph_base.py) of graph 1
         g2: Graph object (from graph_base.py) of graph 2
@@ -142,16 +146,20 @@ class GraphOps():
         R = self.difference(R_union, R_intersection)
         return R
 
+
 gops = GraphOps()
 
-# add labels to the edges after testing
+# when labelled, the third val in edge list is label. Label is a char
 g1 = Graph(V=5,
-           E=6, 
-           edges=[(0, 1),(0,2),(1,2),(1,3),(2,4),(3,4)])
-
-g2 = Graph(V=6, 
            E=6,
-           edges=[(0,1),(0,2),(1,2),(1,5),(2,5),(5,4)])
+           edges=[(0, 1, "a"), (0, 2, "b"), (1, 2, "c"),
+                  (1, 3, "d"), (2, 4, "e"), (3, 4, "f")],
+           is_labelled=True)
+
+g2 = Graph(V=6,
+           E=6,
+           edges=[(0, 1, "a"), (0, 2, "g"), (1, 2, "c"), (1, 5, "h"), (2, 5, "k"), (5, 4,"l")],
+           is_labelled=True)
 
 g1.print_graph_nx()
 g2.print_graph_nx()
@@ -168,5 +176,3 @@ R_difference.print_graph_nx()
 R_ringsum = gops.ringsum(g1, g2)
 R_ringsum.print_graph_nx()
 # plt.show()
-
-
